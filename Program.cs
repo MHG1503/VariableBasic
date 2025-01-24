b5f5bfb3bc8dbb6abba1bdec5f4a3dda277ef8ca
MHG <lengocminh1503@gmail.com>
2025-01-24 10:45:41 +0700

    Create new Student in Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using VariableBasic.Models;
+
 Console.WriteLine("Hello, .NET 02!");
+
+var minh = new Student();
+minh.Id = 1;
+minh.Name = "Minh";
+minh.Email = "m1503@gmail.com";
+
+System.Console.WriteLine(minh.Id);
+System.Console.WriteLine(minh.Name);
+System.Console.WriteLine(minh.Email);
